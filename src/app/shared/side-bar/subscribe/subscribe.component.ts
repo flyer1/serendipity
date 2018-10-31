ebab223444619c525c64bbc40f118ba012b85a20
Jon Kroeker <jon.kroeker2@gmail.com>
2018-10-30 19:04:09 -0500

    Start of subscribe component

---
+++ src/app/shared/side-bar/subscribe/subscribe.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { ComponentBase } from 'src/app/core/component/component-base';
+
+@Component({
+  selector: 'app-subscribe',
+  templateUrl: 'subscribe.component.html',
+  styleUrls: ['subscribe.component.scss']
+})
+
+export class SubscribeComponent extends ComponentBase {
+
+  constructor() {
+    super();
+  }
+
+
+}

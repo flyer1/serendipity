7533292a3fb74a477ebffcda24a2779f19323cc5
Jon Kroeker <jon.kroeker2@gmail.com>
2018-11-24 13:54:23 -0600

    Moved shared files around

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FooterComponent } from './shell/footer/footer.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
-import { TwitterComponent } from './side-bar/twitter/twitter.component';
+import { TwitterComponent } from './twitter/twitter.component';
 import { NavComponent } from './shell/nav/nav.component';
 
 @NgModule({

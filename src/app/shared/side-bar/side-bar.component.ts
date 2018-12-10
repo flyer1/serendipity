b6fe88dec0e2f8ca2532faf2dea3c0330a4e135b
Jon Kroeker <jon.kroeker2@gmail.com>
2018-12-09 20:12:30 -0600

    Work on side bar components.

--- src/app/shared/side-bar/side-bar.component.ts
+++ src/app/shared/side-bar/side-bar.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'app-side-bar',
-    templateUrl: 'side-bar.component.html'
+    templateUrl: './side-bar.component.html',
+    styleUrls: ['./side-bar.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 
 export class SideBarComponent implements OnInit {

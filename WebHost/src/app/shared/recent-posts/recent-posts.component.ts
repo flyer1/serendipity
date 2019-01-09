acbddc7022ccdcc28d98d1df2ffbb0989a65367d
Jon Kroeker <jon.kroeker2@gmail.com>
2019-01-09 16:16:53 -0600

    Move social icons to blog post. WIP on recent posts watching routing

--- WebHost/src/app/shared/recent-posts/recent-posts.component.ts
+++ WebHost/src/app/shared/recent-posts/recent-posts.component.ts
@@ -1,8 +1,10 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 
 import { RecentPostsService } from './services/recent-posts.service';
 import { BlogPostSummary } from 'src/app/features/blog/models/blog-post-summary.model';
 import { RoutingService } from 'src/app/core/routing/routing.service';
+import { takeUntil } from 'rxjs/operators';
+import { ComponentBase } from 'src/app/core/component/component-base';
 
 @Component({
   selector: 'app-recent-posts',
@@ -10,11 +12,18 @@ import { RoutingService } from 'src/app/core/routing/routing.service';
   styleUrls: ['./recent-posts.component.scss'],
   providers: [RecentPostsService]
 })
-export class RecentPostsComponent implements AfterViewInit {
+export class RecentPostsComponent extends ComponentBase implements OnInit, AfterViewInit {
 
   recentPosts: BlogPostSummary[];
 
-  constructor(private recentPostsService: RecentPostsService, private router: RoutingService) { }
+  constructor(private recentPostsService: RecentPostsService, private router: RoutingService) {
+    super();
+  }
+
+  ngOnInit() {
+    // TODO: Add id to the data passed down so the subscriber knows more about where the app is and can react. In this case, compare id with summary.id to mark active item
+    this.router.routeData$.pipe(takeUntil(this.destroy$)).subscribe(d => { console.log(d); });
+  }
 
   ngAfterViewInit() {
     this.recentPostsService.get().subscribe(result => {

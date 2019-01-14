6e20f0619ee279015672d73c58169a365d243f0c
Jon Kroeker <jon.kroeker2@gmail.com>
2019-01-14 17:41:47 -0600

    Key bindings for editor and restricted horizontal resizing

--- WebHost/src/app/shared/editor/editor.component.ts
+++ WebHost/src/app/shared/editor/editor.component.ts
@@ -1,5 +1,6 @@
-import { Component, forwardRef } from '@angular/core';
+import { Component, forwardRef, ViewChild, ElementRef, HostListener } from '@angular/core';
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+import { start } from 'repl';
 
 @Component({
   selector: 'app-editor',
@@ -14,6 +15,8 @@ import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
   ]
 })
 export class EditorComponent implements ControlValueAccessor {
+
+  @ViewChild('input') input: ElementRef;
 
   value: string;
 
@@ -50,18 +53,25 @@ export class EditorComponent implements ControlValueAccessor {
 
   toolbar(tool: string) {
     let newValue = this.value;
+    let selectedValue = '';
+
+    const selection = new Selection(this.input.nativeElement);
+
+    if (selection.length > 0) {
+      selectedValue = this.value.substr(selection.start, selection.length);
+    }
 
     switch (tool) {
       case 'heading':
         break;
       case 'bold':
-        newValue += ' ****';
+        newValue = newValue.substr(0, selection.start) + '**' + selectedValue + '**' + newValue.substr(selection.end);
         break;
       case 'italic':
-        newValue += ' __';
+        newValue = newValue.substr(0, selection.start) + '_' + selectedValue + '_' + newValue.substr(selection.end);
         break;
       case 'link':
-        newValue += '\n![](PASTE_URL_HERE)';
+        newValue = newValue.substr(0, selection.start) + '![](PASTE_URL_HERE)' + newValue.substr(selection.end);
         break;
       case 'code':
         break;
@@ -74,5 +84,40 @@ export class EditorComponent implements ControlValueAccessor {
     }
 
     this.writeValue(newValue);
+  }
+
+  @HostListener('keypress', ['$event']) onKeyDown(event: KeyboardEvent) {
+
+    if (!event.ctrlKey) { return; }
+
+    let handled = true;
+
+    switch (event.code) {
+      case 'KeyB':
+        this.toolbar('bold');
+        break;
+      case 'KeyI':
+        this.toolbar('italic');
+        break;
+      default:
+        handled = false;
+        break;
+    }
+
+    if (handled) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+  }
+}
+
+class Selection {
+  start: number;
+  end: number;
+  get length() { return this.end - this.start; }
+
+  constructor(private element: HTMLTextAreaElement) {
+    this.start = element.selectionStart;
+    this.end = element.selectionEnd;
   }
 }

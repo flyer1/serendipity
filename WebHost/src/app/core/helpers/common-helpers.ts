fa9ab695ceb19969cc6e21f08ae194a0c36c1cd4
Jon Kroeker <jon.kroeker2@gmail.com>
2019-01-24 16:02:07 -0600

    Fix for copy to clipboard for iOS.

--- WebHost/src/app/core/helpers/common-helpers.ts
+++ WebHost/src/app/core/helpers/common-helpers.ts
@@ -4,10 +4,33 @@ export function compare(a: string | Date, b: string | Date, isAsc: boolean = tru
 }
 
 export function copyToClipboard(val: string) {
-  const el = document.createElement('textarea');
-  el.value = val;
-  document.body.appendChild(el);
-  el.select();
-  document.execCommand('copy');
-  document.body.removeChild(el);
+  const textarea = document.createElement('textarea');
+
+  // Place in top-left corner of screen regardless of scroll position.
+  textarea.style.position = 'fixed';
+  textarea.style.top = '0';
+  textarea.style.left = '0';
+  textarea.style.width = '2em';
+  textarea.style.height = '2em';
+  textarea.style.padding = '0';
+  textarea.style.border = 'none';
+  textarea.style.outline = 'none';
+  textarea.style.boxShadow = 'none';
+  textarea.style.background = 'transparent';
+
+  textarea.value = val;
+  document.body.appendChild(textarea);
+  textarea.select();
+
+  try {
+    document.execCommand('copy');
+    return true;
+  } catch (err) {
+    // iOS does not allow to programmatically copy text to the clipboard.
+    console.log('Oops, unable to copy');
+    return false;
+  }
+  finally {
+    document.body.removeChild(textarea);
+  }
 }
